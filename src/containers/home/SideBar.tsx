81a6da186fa7e6a60fef169a4c2c24d73e68e350
fshenma <fshenma@hotmail.com>
2018-11-04 10:32:51 -0500

    init

---
+++ src/containers/home/SideBar.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react'
+import { NavLink } from 'react-router-dom'
+// import MenuList from '@material-ui/core/MenuList'
+import MenuItem from '@material-ui/core/MenuItem'
+// import ListItem from '@material-ui/core/ListItem'
+// import ListIcon from '@material-ui/core/ListItemIcon'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import SvgHome from '@material-ui/icons/Home'
+import SvgFeedback from '@material-ui/icons/Feedback'
+import SvgSettings from '@material-ui/icons/Settings'
+import SvgAccountCircle from '@material-ui/icons/AccountCircle'
+import SvgPeople from '@material-ui/icons/People'
+import { SvgIconProps } from '@material-ui/core/SvgIcon/SvgIcon'
+import Hidden from '@material-ui/core/Hidden'
+import Drawer from '@material-ui/core/Drawer'
+import MenuList from '@material-ui/core/MenuList'
+import classNames from 'classnames'
+
+interface SideBarType {
+    translate: any,
+    profileUrl: string,
+    drawerOpen: boolean,
+    handleDrawerToggle: any,
+    classes?: any
+}
+
+interface MenuItem {
+  path: string,  
+  icon: React.ComponentType<SvgIconProps>,
+  sidebarName: string,
+}
+
+const menuItems: MenuItem[] = [
+  {
+    path: '/',
+    icon: SvgHome,
+    sidebarName: 'sidebar.home',    
+  },
+  {
+    path: '/profile',
+    icon: SvgAccountCircle,
+    sidebarName: 'sidebar.profile',
+  },
+  {
+    path: '/people',
+    icon: SvgPeople,
+    sidebarName: 'sidebar.people',
+  },
+  {
+    path: '/settings',
+    icon: SvgSettings,
+    sidebarName: 'sidebar.settings',
+  },
+  {
+    path: '/feedback',
+    icon: SvgFeedback,
+    sidebarName: 'sidebar.sendFeedback',
+  }
+]
+
+const SideBar: React.SFC<SideBarType> = (props) => {
+     
+  const links = menuItems.map((prop: any, key: any) => {
+    let url: string = prop.path
+
+    if (prop.path === '/profile') {
+      url = props.profileUrl
+    } 
+
+    return (
+      <NavLink to={url} key={key}>
+        <MenuItem style={{ color: 'rgb(117, 117, 117)' }}>
+         <ListItemIcon>
+         <prop.icon />
+         </ListItemIcon>
+         <ListItemText inset primary={props.translate!(prop.sidebarName)} />
+       </MenuItem>
+      </NavLink>      
+    )
+  })
+
+  const sidebar: any = (
+      <>
+        <Hidden mdUp>
+            <Drawer
+              variant='temporary'
+              open={props.drawerOpen}
+              classes={{
+                paper: props.classes.drawerPaper,
+              }}
+              onClose={props.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              <div>
+                <div className={props.classes.drawerHeader} />
+                <MenuList style={{ color: 'rgb(117, 117, 117)', width: '210px' }}>
+                  <Divider />                  
+                  {/* <HomeDrawer translate={this.props.translate} profileUrl={`/${this.props.uid}`} /> */}
+                  {links}
+                </MenuList>
+              </div>
+            </Drawer>
+          </Hidden>
+          <Hidden smDown implementation='js'>
+            <Drawer
+              variant='persistent'
+              open={props.drawerOpen}
+              classes={{
+                paper: props.classes.drawerPaperLarge,
+              }}
+            >
+              <div>
+                <MenuList className={props.classes.menu} style={{ color: 'rgb(117, 117, 117)', width: '210px' }}>
+                {/* <HomeDrawer translate={this.props.translate} profileUrl={`/${this.props.uid}`} /> */}
+                {links}
+                </MenuList>
+              </div>
+            </Drawer>
+          </Hidden>
+      </>
+  )
+
+  return (       
+    <>
+      {sidebar}
+    </>
+    )
+}
+
+export default SideBar
---
+++ src/containers/home/homeStyles.tsx
@@ -0,0 +1,86 @@
+const drawerWidth = 220
+
+const styles = (theme: any) => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    maxWidth: drawerWidth,
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  drawerPaperLarge: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      height: '100%',
+    },
+    top: 70,
+    backgroundColor: '#fafafa',
+    borderRight: 0
+  },
+  menu: {
+    height: '100%',
+  },
+  content: {
+    backgroundColor: 'transparent',
+    width: '100%',
+    flexGrow: 1,
+    padding: theme.spacing.unit * 1,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+  'content-left': {
+    marginLeft: 0,
+  },
+  'content-right': {
+    marginRight: 0,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: 0,
+    [theme.breakpoints.up('md')]: {
+      marginLeft: drawerWidth
+    }
+  },
+  'contentShift-right': {
+    marginRight: 0,
+    [theme.breakpoints.up('md')]: {
+      marginRight: drawerWidth
+    }
+  }
+})
+
+export default styles
